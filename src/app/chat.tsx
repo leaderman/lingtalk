60e620453c9bf8e8dbc06e62925e46a8c1e5c78a
Leaderman <miao.work2010@gmail.com>
2026-03-26 08:55:44 +0800

    feat: 添加 lang: zh-CN 配置

--- src/app/chat.tsx
+++ src/app/chat.tsx
@@ -20,6 +20,7 @@ export default function Chat() {
         ui: {
           base: {
             layout: "mobile",
+            lang: "zh-CN",
           },
           asstBtn: {
             isNeed: false,
